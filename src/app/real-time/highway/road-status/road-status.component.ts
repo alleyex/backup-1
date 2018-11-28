fc5adc44b4eefab2108cc67e575614e71b2979d2
Jung Chun Lin <alleyex@gmail.com>
2018-11-28 12:53:01 +0800

    replace taiwan year and some english noun, olny translate engligh version

--- src/app/real-time/highway/road-status/road-status.component.ts
+++ src/app/real-time/highway/road-status/road-status.component.ts
@@ -14,34 +14,53 @@ export class RoadStatusComponent implements OnInit {
     @Input() status: RoadStatus;
 
     private original: RoadStatus;
+    private english: RoadStatus;
 
     constructor(private googleTranslateService: GoogleTranslateService, private translateService: TranslateService) { }
 
-    ngOnInit() {       
+    ngOnInit() {
         this.original = Object.assign({}, this.status);
 
-        if(this.translateService.currentLang != 'zh-tw'){
+        if (this.translateService.currentLang !== 'zh-tw') {
             this.convert();
         }
 
         this.translateService.onLangChange.subscribe((event) => {
-            if (this.translateService.currentLang == 'zh-tw') {
+            if (this.translateService.currentLang === 'zh-tw') {
                 this.status = Object.assign({}, this.original);
             } else {
-               this.convert();
+                if (this.english) {
+                    this.status = Object.assign({}, this.english);
+                } else {
+                    this.convert();
+                }
             }
         });
     }
 
-    private convert(){
-        this.googleTranslateService.translate(this.original.comment, this.translateService.currentLang).subscribe(result => {
-            this.status.comment = result.data.translations[0].translatedText;
+    private convert() {
+
+        this.english = Object.assign({}, this.status);
+
+        this.googleTranslateService.translate(this.replace(this.original.comment), 'en').subscribe(result => {
+            this.status.comment = this.english.comment = result.data.translations[0].translatedText;
         });
-        this.googleTranslateService.translate(this.original.roadtype, this.translateService.currentLang).subscribe(result => {
-            this.status.roadtype = result.data.translations[0].translatedText;
+
+        this.googleTranslateService.translate(this.original.roadtype, 'en').subscribe(result => {
+            this.status.roadtype = this.english.roadtype = result.data.translations[0].translatedText;
         });
     }
-    
+
+    replace(comment: string): string {
+        comment = comment.replace(/109年/g, '2020年');
+        comment = comment.replace(/108年/g, '2019年');
+        comment = comment.replace(/107年/g, '2018年');
+        comment = comment.replace(/106年/g, '2017年');
+        comment = comment.replace(/中橫公路/g, 'Central Cross-lsland Highway');
+
+        return comment;
+    }
+
     getStatusClass() {
         return {
             'badge-danger': this.status.roadtype === '災變',
@@ -50,6 +69,6 @@ export class RoadStatusComponent implements OnInit {
             'badge-primary': this.status.roadtype === '道路施工',
             'badge-action': this.status.roadtype === '交通管制',
             'badge-info': this.status.roadtype === '其他'
-        }
+        };
     }
 }
